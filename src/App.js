c6ded8d593c470370cce8e21333bb75593ddd3d9
Bobuskiy <bobuskiy.olexander@example.com>
2023-12-03 21:32:30 +0200

    Add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useTranslation } from "react-i18next";
+import "./App.css";
+import useLocalStorage from "./hooks/use-localstorage";
+import i18n from "./i18n";
 
 function App() {
+  const { t } = useTranslation();
+  const [language, setLanguage] = useLocalStorage("language", "ua");
+
+  const handleLanguageChange = (selectedLanguage) => {
+    i18n.changeLanguage(selectedLanguage);
+    setLanguage(selectedLanguage);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{t("Welcome to React")}</h2>
+      <br />
+      <select
+        value={language}
+        onChange={(e) => handleLanguageChange(e.target.value)}
+      >
+        <option value="en">{t("English")}</option>
+        <option value="ua">{t("Ukrainian")}</option>
+      </select>
+      <button className="reload" onClick={() => window.location.reload()}>
+        {t("refresh page")}
+      </button>
     </div>
   );
 }
